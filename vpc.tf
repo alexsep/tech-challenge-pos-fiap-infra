be807dfb55a1ed157352cabb1aa3c5c94f66b6f5
Alex Fonseca <alexxsep@gmail.com>
2024-03-16 17:37:51 -0300

    Criando cluster e nodes

--- vpc.tf
+++ vpc.tf
@@ -5,11 +5,14 @@ resource "aws_vpc" "tc-vpc" {
   }
 }
 
+data "aws_availability_zones" "available" {}
+
 resource "aws_subnet" "subnets" {
-  count = 2
-  availability_zone = "us-east-1a"
+  count = 2   
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id = aws_vpc.tc-vpc.id
   cidr_block = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
   tags = {
     Name ="${var.prefix}-subnet-${count.index}"
   }
